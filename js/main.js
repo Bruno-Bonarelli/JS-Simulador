aec1e13f235daf03a29ca51a933efb0b09fd97e7
bona <brunobonarelli@gmail.com>
2022-03-20 19:49:10 -0300

    primera version

---
+++ js/main.js
@@ -0,0 +1,51 @@
+function comprar () {
+
+    let productoElegido = prompt ('Ingrese el nombre de modelo de anillo que quiere agregar al carrito, si quiere salir en cualquier momento escriba: salir')
+    
+    let total = 0;
+    
+    while (productoElegido != 'salir') {
+    
+        switch ( productoElegido) {
+            case 'infine':
+                total += 600;
+                alert ('Carrito: $' + total);
+                productoElegido = prompt ('Ingrese el modelo del anillo, si quiere salir en cualquier momento escriba: salir')
+                break;
+            
+            case 'amon':
+                total += 650;
+                alert ('Carrito: $' + total);
+                productoElegido = prompt ('Ingrese el modelo del anillo, si quiere salir en cualquier momento escriba: salir');
+                break;
+            
+            case 'waves':
+                total += 790;
+                alert ('Carrito: $' + total);
+                productoElegido = prompt ('Ingrese el modelo del anillo, si quiere salir en cualquier momento escriba: salir');
+            break;
+            
+            case 'block':
+                total += 690;
+                alert ('Carrito: $' + total);
+                productoElegido = prompt ('Ingrese el modelo del anillo, si quiere salir en cualquier momento escriba: salir')
+                break;
+            
+            case 'circular':
+                total += 800;
+                alert ('Carrito: $' + total);
+                productoElegido = prompt ('Ingrese el modelo del anillo, si quiere salir en cualquier momento escriba: salir');
+                break;
+            
+            case 'voronoi':
+                total += 710;
+                alert ('Carrito: $' + total);
+                productoElegido = prompt ('Ingrese el modelo del anillo, si quiere salir en cualquier momento escriba: salir');
+                break;
+            default : 
+                alert ('El producto no existe');
+                productoElegido = prompt ('Ingrese el modelo del anillo, si quiere salir en cualquier momento escriba: salir');
+        }
+    }
+    alert("Carrito: $" + total);
+} 
