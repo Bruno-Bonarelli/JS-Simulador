a7dae83b6911ee362d880b9f34076521c24f88bb
bona <brunobonarelli@gmail.com>
2022-03-25 19:06:15 -0300

    ultimo array y objeto

--- js/main.js
+++ js/main.js
@@ -1,56 +1,3 @@
-/* function comprar () {
-
-    let productoElegido = prompt ('Ingrese el nombre de modelo de anillo que quiere agregar al carrito. Si quiere salir en cualquier momento escriba: salir')
-    
-    let total = 0;
-    
-    while (productoElegido != 'salir') {
-    
-        switch ( productoElegido) {
-            case 'infine':
-                total += 600;
-                alert ('Carrito: $' + total);
-                productoElegido = prompt ('Ingrese el modelo del anillo, si quiere salir en cualquier momento escriba: salir')
-                break;
-            
-            case 'amon':
-                total += 650;
-                alert ('Carrito: $' + total);
-                productoElegido = prompt ('Ingrese el modelo del anillo, si quiere salir en cualquier momento escriba: salir');
-                break;
-            
-            case 'waves':
-                total += 790;
-                alert ('Carrito: $' + total);
-                productoElegido = prompt ('Ingrese el modelo del anillo, si quiere salir en cualquier momento escriba: salir');
-            break;
-            
-            case 'block':
-                total += 690;
-                alert ('Carrito: $' + total);
-                productoElegido = prompt ('Ingrese el modelo del anillo, si quiere salir en cualquier momento escriba: salir')
-                break;
-            
-            case 'circular':
-                total += 800;
-                alert ('Carrito: $' + total);
-                productoElegido = prompt ('Ingrese el modelo del anillo, si quiere salir en cualquier momento escriba: salir');
-                break;
-            
-            case 'voronoi':
-                total += 710;
-                alert ('Carrito: $' + total);
-                productoElegido = prompt ('Ingrese el modelo del anillo, si quiere salir en cualquier momento escriba: salir');
-                break;
-            default : 
-                alert ('El producto no existe');
-                productoElegido = prompt ('Ingrese el modelo del anillo, si quiere salir en cualquier momento escriba: salir');
-        }
-    }
-    alert("Carrito: $" + total);
-}  */
-
-
 class Anillo {
     constructor (nombre, precio) {
         this.nombre = nombre;
@@ -99,5 +46,4 @@ while (anilloElegido != "salir") {
 }
 
 alert ("$" + precioTotal);
-
 
