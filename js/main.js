b6ec50b3cc68a755874dff67fb331035238cf78b
bona <brunobonarelli@gmail.com>
2022-03-25 19:05:01 -0300

    arrays y objetos

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function comprar () {
+/* function comprar () {
 
     let productoElegido = prompt ('Ingrese el nombre de modelo de anillo que quiere agregar al carrito. Si quiere salir en cualquier momento escriba: salir')
     
@@ -48,4 +48,56 @@ function comprar () {
         }
     }
     alert("Carrito: $" + total);
-} 
+}  */
+
+
+class Anillo {
+    constructor (nombre, precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+}
+
+const anillo1 = new Anillo ("infine", 600)
+const anillo2 = new Anillo ("amon", 650)
+const anillo3 = new Anillo ("waves", 790)
+const anillo4 = new Anillo ("block", 690)
+const anillo5 = new Anillo ("circular", 800)
+const anillo6 = new Anillo ("voronoi", 710)
+
+console.log (anillo1)
+
+const anillos = [anillo1, anillo2, anillo3, anillo4, anillo5, anillo6];
+
+function devolverPrecio (anilloElegido) {
+    for (let i = 0; i < anillos.length; i++) {
+        if(anillos[i].nombre == anilloElegido) {
+            return anillos[i].precio;
+        }
+        if (i== 5 && anillos[i]!== anilloElegido) {
+            return -1
+        }
+    }
+}
+
+let precioTotal = 0;
+
+let anilloElegido = prompt ("Introduzca el nombre del anillo");
+
+let precio;
+
+while (anilloElegido != "salir") {
+    precio = devolverPrecio(anilloElegido);
+    if (precio!= -1) {
+        precioTotal += precio;
+    }
+    else {
+        alert ("No existe ese anillo")
+    }
+    alert ("$" + precioTotal);
+    anilloElegido = prompt ("Introduzca el nombre del anillo");
+}
+
+alert ("$" + precioTotal);
+
+
