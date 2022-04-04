fb14e4783a876c9c337b41cbdd0d9cb718926a9c
bona <brunobonarelli@gmail.com>
2022-04-03 22:10:34 -0300

    interaccion con DOM

--- js/main.js
+++ js/main.js
@@ -14,7 +14,35 @@ const anillo6 = new Anillo ("voronoi", 710)
 
 const anillos = [anillo1, anillo2, anillo3, anillo4, anillo5, anillo6];
 
-let precioTotal = 0;
+function Init(){
+    let nombre = prompt("Ingrese su nombre");
+    let apellido = prompt("Ingrese su apellido");
+    const miTitulo = document.getElementById("titulo");
+    miTitulo.innerText = `Bienvenido ${nombre} ${apellido}, estos son los precios:`;
+
+    const contenedor = document.getElementById("divContenedor")
+
+    const miUl = document.createElement("ul");
+    
+    anillos.forEach((anillo)=>{
+        miUl.innerHTML += `<li> Nombre: ${anillo.nombre}<br>
+                           Precio: $${anillo.precio}`
+    });
+
+    contenedor.appendChild(miUl)
+    
+}
+
+/* for (const anillo of anillos) {
+    let contenedor = document.createElement("div")
+    contenedor.innerHTML = `<h3> Anillo: ${anillos.nombre}</h3>
+                            <p>  Precio: $${anillos.precio}</p>`;
+} */
+
+
+
+
+/* let precioTotal = 0;
 
 let anilloElegido = prompt("Introduzca el nombre del anillo");
 
@@ -32,4 +60,4 @@ while (anilloElegido != "salir") {
     anilloElegido = prompt("Introduzca el nombre del anillo que que quiera agregar al carrito. Para abandonar escriba: salir");
 }
 
-alert ("$" + precioTotal);
+alert ("$" + precioTotal); */
